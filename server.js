5e9a30623336918df4fed4450058489f85b0447b
nevosangkane <nevosngkn@gmail.com>
2023-10-04 14:54:32 +0700

    adding a parse application/json and listening express code

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+//parse application/toJSON();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.listen(5000, () => {
+  console.log('App listening on ports 5000.')
+})
+
